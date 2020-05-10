b4b2a0a822e0b1ad6522a0c47c42dd741f12f346
markzhang <zhangwenwu168@gmail.com>
2020-05-10 20:37:41 +0800

    ==和equals的区别

--- src/com/markzhang/Main.java
+++ src/com/markzhang/Main.java
@@ -3,6 +3,7 @@ package com.markzhang;
 /**
  * true
  * false
+ * true
  *   *
  *      * This method will always cache values in the range -128 to 127,
  *      * inclusive, and may cache other values outside of this range.
@@ -15,6 +16,12 @@ package com.markzhang;
  *             return IntegerCache.cache[i + (-IntegerCache.low)];
  *         return new Integer(i);
  *     }
+ *
+ *     ==可以比较基础类型，引用类型，
+ *     对于基础类型比较的是值是否相等
+ *     对于引用类型比较是引用的内存地址是否相等
+ *     equals只用用于引用类型比较，内容是否相同
+ *
  */
 public class Main {
 
@@ -24,5 +31,6 @@ public class Main {
       Integer x = 128,y = 128;
         System.out.println(a == b);
         System.out.println(x == y);
+        System.out.println(a.equals(b));
     }
 }
